50eabb4dc34fc282b95473e16f0901a5f94d96db
AcadeMeData <michael@acade-me.com>
2020-07-22 16:27:00 +0300

    bug fix

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -26,14 +26,13 @@ public class AdventureGame : MonoBehaviour
 
     private void ManageState() {
         var nextStates = state.GetNextStates();
-        if(Input.GetKeyDown(KeyCode.Alpha1)) {
-            state = nextStates[0];
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            state = nextStates[1];
-        } else if (Input.GetKeyDown(KeyCode.Alpha3)){
-            state = nextStates[0];
-        }
 
+        for(int index = 0; index < nextStates.Length; index++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) {
+                state = nextStates[index];
+            }
+        }
+        
         textComponent.text = state.GetStatesStory();
     }
 }
